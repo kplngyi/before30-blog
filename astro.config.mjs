bb1e897f6c742f97653e0c27070649c74f500bdc
lngyi <dingyi609@outlook.com>
2026-04-09 01:09:28 +0800

    Enable Markdown math rendering

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,14 @@
 
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
+import rehypeKatex from 'rehype-katex';
+import remarkMath from 'remark-math';
 
 export default defineConfig({
 	site: 'https://blog.before30.site',
 	integrations: [sitemap()],
+	markdown: {
+		remarkPlugins: [remarkMath],
+		rehypePlugins: [rehypeKatex],
+	},
 });
